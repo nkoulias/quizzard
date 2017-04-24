58f2f632086f1017b1bc11d69cd1b96838b559a7
Nick Koulias <nick.koulias@gmail.com>
2017-04-24 23:41:35 +1000

    24.04.17d

--- quizzard/Questions.swift
+++ quizzard/Questions.swift
@@ -9,5 +9,18 @@
 import SpriteKit
 
 class Questions: SKScene {
+    override func didMove(to view: SKView) {
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            if atPoint(location).name == "back_button" {
+                let play_scene = GameplayScene(fileNamed: "Spin")
+                play_scene?.scaleMode = .aspectFill
+                self.view?.presentScene(play_scene!, transition: SKTransition.doorsOpenVertical(withDuration: 1))
+            }
+        }
+    }
 }
